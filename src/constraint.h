c0a6818a6a28376d70d42f65670e281adaf1742b
S. Chris Colbert <sccolbert@gmail.com>
2013-12-04 19:11:16 -0500

    update ruler to actual 80 chars

--- src/constraint.h
+++ src/constraint.h
@@ -1,10 +1,10 @@
-/*-----------------------------------------------------------------------------
+/*------------------------------------------------------------------------------
 | Copyright (c) 2013, Nucleic Development Team.
 |
 | Distributed under the terms of the Modified BSD License.
 |
 | The full license is in the file COPYING.txt, distributed with this software.
-|----------------------------------------------------------------------------*/
+|-----------------------------------------------------------------------------*/
 #pragma once
 #include <map>
 #include <vector>
