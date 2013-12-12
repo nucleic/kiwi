bfabfc2496724bbb8a4e8e3a12eeb587612df421
Chris Colbert <sccolbert@gmail.com>
2013-12-12 17:36:45 -0500

    update tester file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
-//#include <sys/time.h>
-
 #include "kiwi.h"
-
-#define NOMINMAX
-#include <Windows.h>
-#undef NOMINMAX
-
-
-void doit()
-{
-}
 
 
 int main( int argc, char* argv[] )
@@ -22,51 +11,35 @@ int main( int argc, char* argv[] )
 	kiwi::Constraint c1( 2 * x + 3 * y - 4 * z <= 20 );
 	kiwi::Constraint c2( x <= 10 );
 	kiwi::Constraint c3( y >= 20 );
-	kiwi::Constraint c4( x == 1, kiwi::strength::strong );
-	kiwi::Constraint c5( x == 0.5, kiwi::strength::strong + 10 );
-	kiwi::Constraint c6( x == 0.3 );
-	kiwi::Constraint c7( y == 0.3 );
-
-	LARGE_INTEGER pc1;
-	LARGE_INTEGER pc2;
-	LARGE_INTEGER pf;
+	kiwi::Constraint c4( x == 1, kiwi::strength::weak );
+	kiwi::Constraint c5( x == 0.5, kiwi::strength::medium );
+	kiwi::Constraint c6( x == 0.3, kiwi::strength::strong );
 
 	kiwi::Solver solver;
-	QueryPerformanceFrequency( &pf );
-	QueryPerformanceCounter( &pc1 );
 
-	for( int i=0; i < 100; ++i )
+	for( int i=0; i < 1000; ++i )
 	{
+		solver.reset();
+
 		solver.addConstraint( c1 );
 		solver.addConstraint( c2 );
 		solver.addConstraint( c3 );
 		solver.addConstraint( c4 );
 		solver.addConstraint( c5 );
 		solver.addConstraint( c6 );
-		try
-		{
-			solver.addConstraint( c7 );
-		}
-		catch( const kiwi::UnsatisfiableConstraint& e )
-		{
 
-		}
+		solver.removeConstraint( c4 );
+		solver.removeConstraint( c5 );
+
+		solver.solve();
 	}
 
-	solver.solve();
-	QueryPerformanceCounter( &pc2 );
-
 	solver.dump();
-
 	std::cout << std::endl;
-	std::cout << double( pc2.QuadPart - pc1.QuadPart ) / double( pf.QuadPart ) << std::endl;
-	std::cout << std::endl;
-
 
 	std::cout << "x: " << x.value() << std::endl;
 	std::cout << "y: " << y.value() << std::endl;
 	std::cout << "z: " << z.value() << std::endl;
-
 
 	return 0;
 }
