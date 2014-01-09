249fa863359ea38ac6067ad49f3af807c96d285f
Chris Colbert <sccolbert@gmail.com>
2014-01-09 17:41:17 -0500

    add a version header

---
+++ kiwi/version.h
@@ -0,0 +1,13 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2013, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+#pragma once
+
+#define KIWI_MAJOR_VERSION 0
+#define KIWI_MINOR_VERSION 1
+#define KIWI_MICRO_VERSION 0
+#define KIWI_VERSION "0.1.0"
