67b2818ed4ba3b4d8028ca41084f4a5b6668f119
Chris Colbert <sccolbert@gmail.com>
2014-04-14 12:56:26 -0400

    add an API module
    this is an empty module which slurps in all of the kiwi API dependencies

---
+++ ts/src/kiwi.ts
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+
+/// <reference path="constraint.ts"/>
+/// <reference path="expression.ts"/>
+/// <reference path="solver.ts"/>
+/// <reference path="strength.ts"/>
+/// <reference path="variable.ts"/>
+
+module kiwi { }
