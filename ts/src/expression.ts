c6c326f5aac3fef803c4a4441eadc9f7b9906e64
Chris Colbert <sccolbert@gmail.com>
2014-04-21 14:06:00 -0400

    code formatting

--- ts/src/expression.ts
+++ ts/src/expression.ts
@@ -5,16 +5,22 @@
 |
 | The full license is in the file COPYING.txt, distributed with this software.
 |----------------------------------------------------------------------------*/
-module kiwi {
+
+/// <reference path="../thirdparty/tsu.d.ts"/>
+/// <reference path="maptype.ts"/>
+/// <reference path="variable.ts"/>
+
+module kiwi
+{
 
     /**
-     * An expression of terms and a constant.
+     * An expression of variable terms and a constant.
      *
      * @class
      */
     export
-    class Expression {
-
+    class Expression
+    {
         /**
          * Construct a new Expression.
          *
@@ -26,9 +32,10 @@ module kiwi {
          *
          * The parameters are summed. The tuples are multiplied.
          */
-        constructor(...args: any[]);
-        constructor() {
-            var parsed = parseArgs(arguments);
+        constructor( ...args: any[] );
+        constructor()
+        {
+            var parsed = parseArgs( arguments );
             this._terms = parsed.terms;
             this._constant = parsed.constant;
         }
@@ -38,78 +45,87 @@ module kiwi {
          *
          * This *must* be treated as const.
          */
-        terms(): tsutils.IMap<Variable, number> {
+        terms(): IMap<Variable, number>
+        {
             return this._terms;
         }
 
         /**
          * Returns the constant of the expression.
          */
-        constant(): number {
+        constant(): number
+        {
             return this._constant;
         }
 
         /**
          * Returns the computed value of the expression.
          */
-        value(): number {
+        value(): number
+        {
             var result = this._constant;
-            var iter = this._terms.iter();
-            var pair: tsutils.IPair<Variable, number>;
-            while (pair = iter.next()) {
+            tsu.forEach( this._terms, ( pair ) =>
+            {
                 result += pair.first.value() * pair.second;
-            }
+            } );
             return result;
         }
 
-        private _terms: tsutils.IMap<Variable, number>;
+        private _terms: IMap<Variable, number>;
         private _constant: number;
     }
 
 
     /**
-     * An internal interface for argument parse results.
+     * An internal interface for the argument parse results.
      */
-    interface IParseResult {
-        terms: tsutils.IMap<Variable, number>;
+    interface IParseResult
+    {
+        terms: IMap<Variable, number>;
         constant: number;
     }
 
 
     /**
-     * The map type used to create the term map.
-     */
-    var MapType = tsutils.AssocArray;
-
-
-    /**
      * An internal argument parsing function.
      */
-    function parseArgs(args: IArguments): IParseResult {
+    function parseArgs( args: IArguments ): IParseResult
+    {
         var constant = 0.0;
         var factory = () => 0.0;
-        var terms = new MapType<Variable, number>(Variable.Compare);
-        for (var i = 0, n = args.length; i < n; ++i) {
-            var item = args[i];
-            if (typeof item === "number") {
-                constant += <number>item;
-            } else if (item instanceof Variable) {
-                terms.setDefault(<Variable>item, factory).second += 1.0;
-            } else if (item instanceof Array) {
-                if (item.length !== 2) {
-                    throw new Error("array must have length 2");
+        var terms = createMap<Variable, number>( Variable.Compare );
+        for( var i = 0, n = args.length; i < n; ++i )
+        {
+            var item = args[ i ];
+            if( typeof item === "number" )
+            {
+                constant += item;
+            }
+            else if( item instanceof Variable )
+            {
+                terms.setDefault( item, factory ).second += 1.0;
+            }
+            else if( item instanceof Array )
+            {
+                if( item.length !== 2 )
+                {
+                    throw new Error( "array must have length 2" );
                 }
-                var value: number = item[0];
-                var variable: Variable = item[1];
-                if (typeof value !== "number") {
-                    throw new Error("array item 0 must be a number");
+                var value: number = item[ 0 ];
+                var variable: Variable = item[ 1 ];
+                if( typeof value !== "number" )
+                {
+                    throw new Error( "array item 0 must be a number" );
                 }
-                if (!(variable instanceof Variable)) {
-                    throw new Error("array item 1 must be a variable");
+                if( !( variable instanceof Variable ) )
+                {
+                    throw new Error( "array item 1 must be a variable" );
                 }
-                terms.setDefault(variable, factory).second += value;
-            } else {
-                throw new Error("invalid Expression argument: " + item);
+                terms.setDefault( variable, factory ).second += value;
+            }
+            else
+            {
+                throw new Error( "invalid Expression argument: " + item );
             }
         }
         return { terms: terms, constant: constant };
