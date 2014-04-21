85f071f71054fd2376f9198a8cd9287ff01ae85b
Chris Colbert <sccolbert@gmail.com>
2014-04-21 14:04:24 -0400

    add a module for "typedef"ing a map type

---
+++ ts/src/maptype.ts
@@ -0,0 +1,25 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+
+/// <reference path="../thirdparty/tsu.d.ts"/>
+
+module kiwi
+{
+
+    export
+    interface IMap<T, U>
+        extends tsu.AssociativeArray<T, U> { }
+
+
+    export
+    function createMap<T, U>( compare: tsu.ICompare<T, T> ): IMap<T, U>
+    {
+        return new tsu.AssociativeArray<T, U>( compare );
+    }
+
+}
