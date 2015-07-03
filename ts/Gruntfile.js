b718b9049b13a689247ea0ee28931747d5cc9c06
IjzerenHein <hrutjes@gmail.com>
2015-07-03 15:04:08 +0200

    Added simple build toolchain using grunt (linting disabled for now)

---
+++ ts/Gruntfile.js
@@ -0,0 +1,68 @@
+/*global module:false*/
+/*eslint strict:false, quotes: [2, "single"] */
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    tslint: {
+      options: {
+        configuration: '' //grunt.file.readJSON('tslint.json')
+      },
+      files: {
+        src: ['src/*.ts']
+      }
+    },
+    /*jsdoc2md: {
+      output: {
+        options: {
+          'global-index-format': 'none',
+          'module-index-format': 'none'
+        },
+        src: 'bin/kiwi.js',
+        dest: 'docs/Kiwi.md'
+      }
+    },*/
+    exec: {
+      build: 'tsc --noImplicitAny -d -out bin/kiwi.js src/kiwi.ts'
+    },
+    uglify: {
+      dist: {
+        src: './bin/kiwi.js',
+        dest: './bin/kiwi.min.js'
+      }
+    },
+    usebanner: {
+      minified: {
+        options: {
+          position: 'top',
+          banner:
+            '/*-----------------------------------------------------------------------------\n' +
+            '| Copyright (c) 2014, Nucleic Development Team.\n' +
+            '|\n' +
+            '| Distributed under the terms of the Modified BSD License.\n' +
+            '|\n' +
+            '| The full license is in the file COPYING.txt, distributed with this software.\n' +
+            '-----------------------------------------------------------------------------*/'
+        },
+        files: {
+          src: ['bin/kiwi.min.js']
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
+  grunt.loadNpmTasks('grunt-tslint');
+  grunt.loadNpmTasks('grunt-banner');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-exec');
+
+  // Tasks
+  grunt.registerTask('lint', ['tslint']);
+  //grunt.registerTask('doc', ['jsdoc2md']);
+  //grunt.registerTask('test', ['exec:test']);
+  grunt.registerTask('build', ['exec:build']);
+  grunt.registerTask('minify', ['uglify', 'usebanner']);
+  grunt.registerTask('default', ['build', 'lint', 'minify']);
+};
