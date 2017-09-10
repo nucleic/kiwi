aa85dcbee6b24e9e2af0f3c1da0e0547fcc81b4f
Matthieu Dartiailh <marul@laposte.net>
2017-09-10 18:36:46 +0200

    py: hopefully

--- py/util.h
+++ py/util.h
@@ -56,9 +56,11 @@ convert_to_strength( PyObject* value, double& out )
     if( PyString_Check( value ) | PyUnicode_Check( value ))
     {
       if( PyUnicode_Check( value ) )
+      {
           ascii_str = PyUnicode_AsASCIIString( value );
           str = PyString_AS_STRING( ascii_str );
           Py_DECREF( ascii_str );
+      }
       else
           str = PyString_AS_STRING( value ) ;
 #endif
