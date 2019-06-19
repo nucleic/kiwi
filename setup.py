3c0a0d978c0ce5c81c390dcfbd254548214f3811
Paul Kernfeld <paulkernfeld@users.noreply.github.com>
2019-06-19 10:43:55 -0400

    Remove setuptools from install_requires
    This should fix #72

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
           'Programming Language :: Python :: Implementation :: CPython',
       ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['setuptools'],
+    install_requires=[],
     ext_modules=ext_modules,
     cmdclass={'build_ext': BuildExt},
 )
